72afb042cf0793c1e4e53ef0bfc1074a4a634214
JovemDevv <anacarolinesan18@gmail.com>
2022-10-12 12:14:19 -0300

    cotação

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import PySimpleGUI as sg
+from cotacao import pegar_cotacoes
+
+sg.theme('Reds')
 
 layout = [
-    [sg.Text("Pegar cotação da bolsa")],
-    [sg.InputText()],
-    [sg.Button("Pegar cotação"), sg.Button("Cancelar")],
-    [sg.text("")],
+    [sg.Text("Moeda", font = 'arial 12 bold')],
+    [sg.InputText(key="nome_cotacao")],
+    [sg.Button('Cotação', font = 'arial 10 bold'), sg.Button("Cancelar", font = 'arial 10 bold')],
+    [sg.Text("", key='texto_cotacao'), sg.Text("Desenvolvido por Ana Caroline Vasconcellos", font="arial 8")],
+    
 ]
+
+janela = sg.Window("Sistema de Cotações",  layout)
+
+#requisição
+while True:
+    evento, valores = janela.read()
+    if evento == sg.WIN_CLOSED or evento == "Cancelar":
+        break
+    if evento == "Cotação":
+        codigo_cotacao = valores["nome_cotacao"]
+        cotacao = pegar_cotacoes(codigo_cotacao)
+        janela["texto_cotacao"].update(f"A cotação do {codigo_cotacao} é de R${cotacao}")
+
+janela.close()
